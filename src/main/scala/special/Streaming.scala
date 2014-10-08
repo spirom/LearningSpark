837fc5b37c1e214f6995edae127075d8dc0e688a
Spiro Michaylov <spiro@michaylov..com>
2014-10-07 22:51:01 -0700

    Adding streaming example

--- src/main/scala/special/Streaming.scala
+++ src/main/scala/special/Streaming.scala
@@ -1,12 +1,20 @@
 package special
 
 import java.io.File
+import org.apache.spark.streaming._
 
 import org.apache.spark.{SparkContext, SparkConf}
 
 import scala.util.Random
 
-object Streaming {
+class FileMaker {
+
+  val root = new File("c:" + File.separator + "temp" + File.separator + "streamFiles")
+  makeExist(root)
+  val prep = new File(root.getAbsolutePath() + File.separator + "prep")
+  makeExist(prep)
+  val dest = new File(root.getAbsoluteFile() + File.separator + "dest")
+  makeExist(dest)
 
   def writeOutput(f: File) : Unit = {
     for (i <- 1 to 100) {
@@ -19,27 +27,33 @@ object Streaming {
     }
   }
 
-  def makeExist(dir: String, empty: Boolean) : Unit = {
-    // TODO: make sure it exists
-    // TODO: clean if needed
+  def makeExist(dir: File) : Unit = {
+    dir.mkdir()
   }
 
-  def makeFiles() : Unit = {
-    val root = "c:/temp/streamFiles"
-    makeExist(root, false)
-    val prep = root + "/prep"
-    makeExist(prep, false)
-    val dest = root + "/dest"
-    makeExist(dest, true)
+  def makeFiles() = {
     for (n <- 1 to 10) {
-      val localName = "f_" + n + ".txt"
-      val f = new File(prep + "/" + localName)
-      f renameTo new File(dest + "/" + localName)
+      val f = File.createTempFile("Spark_", ".txt", prep)
+      writeOutput(f)
+      val nf = new File(dest + File.separator + f.getName())
+      f renameTo nf
+      nf.deleteOnExit()
     }
   }
+}
+
+object Streaming {
+
+
 
   def main (args: Array[String]) {
     val conf = new SparkConf().setAppName("Streaming").setMaster("local[4]")
     val sc = new SparkContext(conf)
+    val ssc = new StreamingContext(sc, Seconds(1))
+    val fm = new FileMaker()
+    val stream =  ssc.textFileStream(fm.dest.getAbsolutePath())
+    stream.foreachRDD(r => println(r.count()))
+    fm.makeFiles()
+    Thread.sleep(10000)
   }
 }
