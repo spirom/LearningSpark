6e586c6c8d1e7300fb6eb914cce8fdefe5066fba
Spiro Michaylov <spiro@michaylov..com>
2014-11-10 22:35:30 -0800

    Remove accidentally included files

--- src/scripts/run_config1.bat
+++
@@ -1,7 +0,0 @@
-
-set CONFIG="C:\Projects\LearningSpark\src\config"
-
-set MONGOBIN="c:\Program Files\MongoDB 2.6 Standard\bin"
-set DBROOT="c:\mongoinst\data"
-
-%MONGOBIN%\mongod.exe --config %CONFIG%\config1.cfg
--- src/scripts/run_mongos.bat
+++
@@ -1,5 +0,0 @@
-set CONFIG="C:\Projects\LearningSpark\src\config"
-
-set MONGOBIN="c:\Program Files\MongoDB 2.6 Standard\bin"
-
-%MONGOBIN%\mongos.exe --configdb localhost:27032 --port 27033
