9f5f14304875c6d164ce93a9ff091f90fd4d12e9
Spiro Michaylov <spiro@michaylov.com>
2014-12-21 18:59:05 -0800

    Schema conversion

--- src/main/scala/sql/SchemaConversion.scala
+++ src/main/scala/sql/SchemaConversion.scala
@@ -1,29 +1,40 @@
 package sql
 
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkContext, SparkConf}
 
 import org.apache.spark.sql._
 
 import scala.collection.immutable
 
-case class OCC(i: Integer)
-case class CC(i: Integer, m: Map[Integer,Integer])
+case class CC(i: Integer)
 
-// TODO: show how to pull data out of a SchemaRDD, convert to some
-// TODO: case class, and back to SchemaRDD again
+
 
 object SchemaConversion {
-  def main (args: Array[String]) {
-    val conf = new SparkConf().setAppName("Parquet").setMaster("local[4]")
-    val sc = new SparkContext(conf)
-    val sqlContext = new SQLContext(sc)
+  val conf = new SparkConf().setAppName("SchemaConversion").setMaster("local[4]")
+  val sc = new SparkContext(conf)
+  val sqlContext = new SQLContext(sc)
 
+  def toSchemaRDD(o: RDD[CC]) : SchemaRDD = {
+    val schema = StructType(
+      Seq(StructField("i", IntegerType, true)))
+    val rows = o.map(cc => Row(cc.i))
+    sqlContext.applySchema(rows, schema)
+  }
+
+  def main (args: Array[String]) {
 
 
     val nums = 1 to 100
-    val data = sc.parallelize(nums.map(i => CC(i, new immutable.HashMap[Integer, Integer]())), 4)
-    val sdata = sqlContext.createSchemaRDD(data)
-    sdata.foreach(println)
+    val data = sc.parallelize(nums.map(i => CC(i)), 4)
+    val sdata = toSchemaRDD(data)
+
+    sdata.registerTempTable("mytable")
+
+    val results = sqlContext.sql("SELECT COUNT(i) FROM mytable WHERE i > 50")
+
+    results.foreach(println)
 
 
 
