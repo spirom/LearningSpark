e7dc1d34e1917512c196a5fab74ac43a781e7c27
Spiro Michaylov <spiro@michaylov..com>
2014-12-23 08:00:32 -0800

    More PrunedFilteredScan

--- src/main/scala/sql/RelationProviderFilterPushdown.scala
+++ src/main/scala/sql/RelationProviderFilterPushdown.scala
@@ -5,7 +5,7 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.catalyst.expressions.Row
 import org.apache.spark.sql.catalyst.types.{IntegerType, StructField, StructType}
-import org.apache.spark.sql.sources.{Filter, PrunedFilteredScan, RelationProvider, TableScan}
+import org.apache.spark.sql.sources._
 
 import scala.collection.mutable.ListBuffer
 
@@ -26,8 +26,41 @@ import scala.collection.mutable.ListBuffer
 //
 case class MyPFTableScan(count: Int, partitions: Int)
                       (@transient val sqlContext: SQLContext)
-  extends PrunedFilteredScan
-{
+  extends PrunedFilteredScan {
+  private val attributes = Seq("val", "squared", "cubed")
+
+  private def getFilterAttribute(f: Filter): String = {
+    f match {
+      case EqualTo(attr, v) => attr
+      case GreaterThan(attr, v) => attr
+      case LessThan(attr, v) => attr
+      case GreaterThanOrEqual(attr, v) => attr
+      case LessThanOrEqual(attr, v) => attr
+      case In(attr, vs) => attr
+    }
+  }
+
+  private def satisfiesAllFilters(value: Int, filters: Array[Filter]): Boolean = {
+    filters.forall({
+      case EqualTo(attr, v) => value == v.asInstanceOf[Int]
+      case GreaterThan(attr, v) => v.asInstanceOf[Int] > value
+      case LessThan(attr, v) => v.asInstanceOf[Int] < value
+      case GreaterThanOrEqual(attr, v) => v.asInstanceOf[Int] >= value
+      case LessThanOrEqual(attr, v) => v.asInstanceOf[Int] <= value
+      case In(attr, vs) => vs.exists(v => v.asInstanceOf[Int] == value)
+    })
+  }
+
+  private def splitAttributes(filters: Array[Filter], names: Seq[String]):
+    Map[String, Array[Filter]] =
+  {
+    val attrToFilters = filters
+      .map(f => (getFilterAttribute(f), f))
+      .groupBy(attrFilter => attrFilter._1)
+      .mapValues(a => a.map(p => p._2))
+    attrToFilters
+  }
+
   val schema: StructType = StructType(Seq(
     StructField("val", IntegerType, nullable = false),
     StructField("squared", IntegerType, nullable = false),
@@ -47,9 +80,18 @@ case class MyPFTableScan(count: Int, partitions: Int)
     r
   }
 
+  // NOTE: you're not actually guaranteed to get filters -- in particular
+  // you won't get them if the filter you specified can't be turned into a
+  // conjunction fo simple filters -- you might expect to get SOME filters in
+  // such cases, but it seems you won't
   def buildScan(requiredColumns: Array[String], filters: Array[Filter]): RDD[Row] = {
-    val values = (1 to count).map(i => makeRow(i, requiredColumns))
-    sqlContext.sparkContext.parallelize(values, partitions)
+    // get the filters organized by the full set of attributes
+    // (could probably use requiredColumns as Spark SQL is likely to ask for
+    // all columns the query filters on, just so it can reapply the filters itself)
+    val attrToFilters = splitAttributes(filters, attributes)
+    val keys = (1 to count).filter(v => satisfiesAllFilters(v, attrToFilters("val")))
+    val rows = keys.map(i => makeRow(i, requiredColumns))
+    sqlContext.sparkContext.parallelize(rows, partitions)
   }
 
 }
@@ -84,12 +126,13 @@ object RelationProviderFilterPushdown {
       """.stripMargin)
 
     // query the table we registered, using its column names
+    // NOTE: requests the columns val, cubed, squared in that order!
     val data =
       sqlContext.sql(
         s"""
-          |SELECT val, cubed, val
+          |SELECT val, cubed
           |FROM dataTable
-          |WHERE val <= 10 OR squared >= 900
+          |WHERE val <= 40 AND squared >= 900
           |ORDER BY val
         """.stripMargin)
     data.foreach(println)
