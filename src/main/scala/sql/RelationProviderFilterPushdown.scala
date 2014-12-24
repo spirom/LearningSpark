a20b1f93e54398fef596ca571262e5202214b804
Spiro Michaylov <spiro@michaylov..com>
2014-12-24 07:53:07 -0800

    PrunedFilteredScan works

--- src/main/scala/sql/RelationProviderFilterPushdown.scala
+++ src/main/scala/sql/RelationProviderFilterPushdown.scala
@@ -7,10 +7,10 @@ import org.apache.spark.sql.catalyst.expressions.Row
 import org.apache.spark.sql.catalyst.types.{IntegerType, StructField, StructType}
 import org.apache.spark.sql.sources._
 
-import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.{HashMap, ListBuffer}
+import scala.collection.parallel.mutable
 
-// TODO: produce columns in the right order
-// TODO: filter the rows
+// TODO: reorganize to look like external DB?
 // TODO: don't _completely_ filter the rows, to show its not necessary
 
 //
@@ -20,16 +20,31 @@ import scala.collection.mutable.ListBuffer
 // configuration parameters.
 //
 
-//
-// Extending TableScan allows us to describe the schema and
-// provide the rows when requested
-//
-case class MyPFTableScan(count: Int, partitions: Int)
-                      (@transient val sqlContext: SQLContext)
-  extends PrunedFilteredScan {
-  private val attributes = Seq("val", "squared", "cubed")
+class MegaFilter(filters: Array[Filter]) {
 
-  private def getFilterAttribute(f: Filter): String = {
+  val attrToFilters: Map[String, Array[Filter]] = filters
+    .map(f => (MegaFilter.getFilterAttribute(f), f))
+    .groupBy(attrFilter => attrFilter._1)
+    .mapValues(a => a.map(p => p._2))
+
+  def apply(attr: String, v: Int): Boolean = {
+    val filters = attrToFilters.getOrElse(attr, new Array[Filter](0))
+    MegaFilter.satisfiesAll(v, filters)
+  }
+
+  def apply(r: HashMap[String, Int]): Boolean = {
+    r.forall({
+      case (attr, v) => {
+        val filters = attrToFilters.getOrElse(attr, new Array[Filter](0))
+        MegaFilter.satisfiesAll(v, filters)
+      }
+    })
+  }
+
+}
+
+object MegaFilter {
+   private def getFilterAttribute(f: Filter): String = {
     f match {
       case EqualTo(attr, v) => attr
       case GreaterThan(attr, v) => attr
@@ -40,26 +55,25 @@ case class MyPFTableScan(count: Int, partitions: Int)
     }
   }
 
-  private def satisfiesAllFilters(value: Int, filters: Array[Filter]): Boolean = {
+  def satisfiesAll(value: Int, filters: Array[Filter]): Boolean = {
     filters.forall({
       case EqualTo(attr, v) => value == v.asInstanceOf[Int]
-      case GreaterThan(attr, v) => v.asInstanceOf[Int] > value
-      case LessThan(attr, v) => v.asInstanceOf[Int] < value
-      case GreaterThanOrEqual(attr, v) => v.asInstanceOf[Int] >= value
-      case LessThanOrEqual(attr, v) => v.asInstanceOf[Int] <= value
-      case In(attr, vs) => vs.exists(v => v.asInstanceOf[Int] == value)
+      case GreaterThan(attr, v) => value > v.asInstanceOf[Int]
+      case LessThan(attr, v) => value < v.asInstanceOf[Int]
+      case GreaterThanOrEqual(attr, v) => value >= v.asInstanceOf[Int]
+      case LessThanOrEqual(attr, v) => value <= v.asInstanceOf[Int]
+      case In(attr, vs) => vs.exists(v => value == v.asInstanceOf[Int])
     })
   }
+}
 
-  private def splitAttributes(filters: Array[Filter], names: Seq[String]):
-    Map[String, Array[Filter]] =
-  {
-    val attrToFilters = filters
-      .map(f => (getFilterAttribute(f), f))
-      .groupBy(attrFilter => attrFilter._1)
-      .mapValues(a => a.map(p => p._2))
-    attrToFilters
-  }
+//
+// Extending TableScan allows us to describe the schema and
+// provide the rows when requested
+//
+case class MyPFTableScan(count: Int, partitions: Int)
+                      (@transient val sqlContext: SQLContext)
+  extends PrunedFilteredScan {
 
   val schema: StructType = StructType(Seq(
     StructField("val", IntegerType, nullable = false),
@@ -70,12 +84,17 @@ case class MyPFTableScan(count: Int, partitions: Int)
   // NOTE: it's not enough to merely produce the right columns:
   // they must be produced in the order requested, which may be different
   // from the order specified when returning the schema
-  private def makeRow(i: Int, requiredColumns: Array[String]): Row = {
-    val l = ListBuffer[Int]()
-    if (requiredColumns.contains("val")) l.append(i)
+  private def makeRow(i: Int, requiredColumns: Array[String]): HashMap[String, Int] = {
+    val m = new HashMap[String, Int]()
+    if (requiredColumns.contains("val")) m += ("val" -> i)
+    if (requiredColumns.contains("squared")) m += ("squared" -> i*i)
+    if (requiredColumns.contains("cubed")) m += ("cubed" -> i*i*i)
+    m
+  }
 
-    if (requiredColumns.contains("cubed")) l.append(i*i*i)
-    if (requiredColumns.contains("squared")) l.append(i*i)
+  // get the columns in the right order and wrap up as a Row
+  private def wrapRow(m: HashMap[String, Int], requiredColumns: Array[String]): Row = {
+    val l = requiredColumns.map(c => m(c))
     val r = Row.fromSeq(l)
     r
   }
@@ -88,9 +107,12 @@ case class MyPFTableScan(count: Int, partitions: Int)
     // get the filters organized by the full set of attributes
     // (could probably use requiredColumns as Spark SQL is likely to ask for
     // all columns the query filters on, just so it can reapply the filters itself)
-    val attrToFilters = splitAttributes(filters, attributes)
-    val keys = (1 to count).filter(v => satisfiesAllFilters(v, attrToFilters("val")))
-    val rows = keys.map(i => makeRow(i, requiredColumns))
+    val megaFilter = new MegaFilter(filters)
+    val keys = (1 to count).filter(v => megaFilter.apply("val", v))
+    val rows = keys
+      .map(i => makeRow(i, requiredColumns))
+      .filter(r => megaFilter.apply(r))
+      .map(r => wrapRow(r, requiredColumns))
     sqlContext.sparkContext.parallelize(rows, partitions)
   }
 
