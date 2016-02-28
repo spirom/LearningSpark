ac679faedbce1c95e947b1938c9636375f15e2b1
Spiro Michaylov <spiro@michaylov.com>
2016-02-28 11:24:04 -0800

    Core Spark solution to SQL "choose within groups" problem.

--- src/main/scala/experiments/ChooseWithinGroups.scala
+++ src/main/scala/experiments/ChooseWithinGroups.scala
@@ -1,5 +1,6 @@
 package experiments
 
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{Column, SQLContext}
 import org.apache.spark.{SparkConf, SparkContext}
@@ -40,5 +41,40 @@ object ChooseWithinGroups {
       "inner").select("id", "first", "last", "country", "age")
 
     maxAgeAll.show()
+
+    //
+    // And now for a core Spark solution
+    //
+
+    type Payload = (String, String, String, Int)
+
+    val pairs: RDD[(Int, Payload)] = peopleRows.map({
+      case (id: Int, first: String, last: String, country: String, age: Int) =>
+        (id, (first, last, country, age))
+    }
+    )
+
+    def add(acc: Option[Payload], rec: Payload): Option[Payload] = {
+      acc match {
+        case None => Some(rec)
+        case Some(previous) => if (rec._4 > previous._4) Some(rec) else acc
+      }
+    }
+
+    def combine(acc1: Option[Payload], acc2: Option[Payload]): Option[Payload] = {
+      (acc1, acc2) match {
+        case (None, None) => None
+        case (None, _) => acc2
+        case (_, None) => acc1
+        case (Some(p1), Some(p2)) => if (p1._4 > p2._4) acc1 else acc2
+      }
+    }
+
+    val start: Option[Payload] = None
+
+    val withMax: RDD[(Int, Option[Payload])] =
+      pairs.aggregateByKey(start)(add, combine)
+
+    withMax.collect().foreach(println)
 
   }}
